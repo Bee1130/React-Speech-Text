7279bff0f17d9fb96d566780bf03430587a4fcc7
Bee1130 <constructive.bee@gmail.com>
2023-05-16 01:03:25 +0900

    :sparkles: added text-to-speech feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './microphone.png';
+import React, { useEffect, useState } from 'react';
+import microphoneImg from './assets/microphone.png';
+import headphoneImg from './assets/headphone.png';
 import './App.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { AwesomeButton } from "react-awesome-button";
 import 'react-awesome-button/dist/styles.css';
+import { KeyboardEvent } from 'react';
 
 function App() {
+
+  const [userText, setUserText] = useState<string>('');
 
   const {
     transcript,
@@ -14,21 +18,46 @@ function App() {
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
 
+  useEffect(() => {
+    setUserText(transcript);
+  }, [transcript]);
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Your Browser doesn't support Speech to Text</span>
   }
-  console.log(transcript)
+
+  const handleTextToSpeechClick = () => {
+    const value = new SpeechSynthesisUtterance(userText);
+    window.speechSynthesis.speak(value);
+  }
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleTextToSpeechClick();
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <p>Microphone: {listening ? 'on' : 'off'}</p>
-          <AwesomeButton type="primary" className='btn-control' onPress={() => SpeechRecognition.startListening()}>Start</AwesomeButton>
-          <AwesomeButton type="danger" className='btn-control' onPress={() => SpeechRecognition.stopListening()}>Stop</AwesomeButton>
-          <AwesomeButton type="secondary" className='btn-control' onPress={resetTranscript}>Reset</AwesomeButton>
-          <p>{transcript}</p>
+          <img src={microphoneImg} className="App-logo" alt="microphone" />
+          <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <AwesomeButton type="primary" className='btn-control' onPress={() => SpeechRecognition.startListening()}>Start</AwesomeButton>
+            <AwesomeButton type="danger" className='btn-control' onPress={() => SpeechRecognition.stopListening()}>Stop</AwesomeButton>
+            <AwesomeButton type="secondary" className='btn-control' onPress={resetTranscript}>Reset</AwesomeButton>
+            <p>{transcript}</p>
+            <input 
+              type="text" 
+              className="App-Input" 
+              placeholder="Text" 
+              value={userText}
+              onChange={e => setUserText(e.target.value)}
+              onKeyDownCapture={handleKeyPress}
+            />
+            <AwesomeButton style={{width: '100%'}} type="primary" onPress={handleTextToSpeechClick}>Start</AwesomeButton>
+          </div>
         </div>
       </header>
     </div>
